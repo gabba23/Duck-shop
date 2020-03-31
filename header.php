c4c45eb13205692a8cc1c83fd86f5d5f18c0fbeb
unknown <gabrieliuc11@gmail.com>
2020-03-31 20:23:14 +0200

    Added contact form, only HTML, not functional as of now

--- header.php
+++ header.php
@@ -77,7 +77,7 @@
                             <a class="nav-link" href="about.php">About</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="about.php">Contact us</a>
+                            <a class="nav-link" href="contact.php">Contact us</a>
                         </li>
                     </ul>
         
