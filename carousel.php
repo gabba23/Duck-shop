2e4540307e5866911374de02e0655a5789fba0ea
Olsonika <alek2257@easv365.dk>
2020-04-30 08:58:35 +0200

    Shopping cart added + Recaptcha update

---
+++ carousel.php
@@ -0,0 +1,39 @@
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+    </ol>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="./assets/images/duck_1.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+            <img src="./assets/images/duck_2.jpg" class="d-block w-100" alt="...">
+
+        </div>
+
+        <div class="carousel-item">
+            <img src="./assets/images/duck_3.png" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+            <img src="./assets/images/duck_4.jpg" class="d-block w-100" alt="...">
+        </div>
+    </div>
+    <a class="carousel-control-prev light-gray" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next light-gray" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
+
+<section class="jumbotron text-center">
+    <div class="container">
+        <h1 class="jumbotron-heading">Rubber ducks</h1>
+        <p class="lead text-muted mb-0">Get our ducks now, because they are great!</p>
+    </div>
+</section>
